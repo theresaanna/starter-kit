85e08ff95e5f0c713923a1bf4fc04c101fbb7e72
Majd Taby <mtaby@me.com>
2011-08-15 15:55:51 -0700

    change hello world to make it pinchable

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 var App = SC.Application.create();
 
 App.MyView = SC.View.extend({
+  pinchChange: function(gestureRecognizer) {
+    this.$().css('scale',function(index, value) {
+      return gestureRecognizer.get('scale') * value;
+    })
+  },
+  
   mouseDown: function() {
     window.alert("hello world!");
   }
