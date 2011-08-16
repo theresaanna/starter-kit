3858dac8a54acb9ad3961d773a2aad04e50ddff6
Majd Taby <mtaby@me.com>
2011-08-15 19:20:18 -0700

    add pan to the default template

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 var App = SC.Application.create();
 
 App.MyView = SC.View.extend({
-  pinchChange: function(gestureRecognizer) {
+  pinchChange: function(rec) {
     this.$().css('scale',function(index, value) {
-      return gestureRecognizer.get('scale') * value;
-    })
+      return rec.get('scale') * value
+    });
   },
+
+  panOptions: {
+    numberOfRequiredTouches: 2
+  },
+
+  panChange: function(rec) {
+    var val = rec.get('translation');
   
-  mouseDown: function() {
-    window.alert("hello world!");
+    this.$().css({
+      translateX: '%@=%@'.fmt((val.x < 0)? '-' : '+',Math.abs(val.x)),
+      translateY: '%@=%@'.fmt((val.y < 0)? '-' : '+',Math.abs(val.y))
+    });
   }
 });
